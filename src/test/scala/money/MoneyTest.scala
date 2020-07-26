b11304b4edb1c5c894f64bdf3d962a718f5a663d
emag <lanabe.lanabe@gmail.com>
2020-07-27 00:01:35 +0900

    Add Scalafmt

--- src/test/scala/money/MoneyTest.scala
+++ src/test/scala/money/MoneyTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 class MoneyTest extends AnyFlatSpec {
 
   "Dollar" should "be able to multiply" in {
-    val five = new Dollar(5)
+    val five    = new Dollar(5)
     var product = five.times(2)
     assert(product.amount === 10)
     product = five.times(3)
